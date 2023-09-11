dc4390c7bd634412a46501f12711205dbf3ce07f
OverInk <dostoevskiy201@mail.ru>
2023-09-11 16:07:56 +0300

    New

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import './index.css';
+
 
 
 
 function App() {
 	return (
-		<div className="App">
-			Shop
+		<div className="wrapper">
+			<Header />
+
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import cl from './style.css';
+
+export default function Header() {
+	return (
+		<header>
+			<div>
+				<div className='logo'>Home Staff <span className='new-font'>Off</span></div>
+			</div>
+			<div className='presentation'></div>
+		</header>
+	)
+}
