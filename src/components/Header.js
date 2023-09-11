e5c3b3e9c2be38d9a58482bae7572bfc92cd8a07
OverInk <dostoevskiy201@mail.ru>
2023-09-11 23:20:11 +0300

    New

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,13 @@ export default function Header() {
 	return (
 		<header>
 			<div>
-				<div className='logo'>Home Staff <span className='new-font'>Off</span></div>
+				<div className='logo'>Home Staff <span className='new-font'>Off</span>
+					<ul className='nav'>
+						<li>NO about yourself</li>
+						<li>Contankts</li>
+						<li>KabiNET</li>
+					</ul>
+				</div>
 			</div>
 			<div className='presentation'></div>
 		</header>
